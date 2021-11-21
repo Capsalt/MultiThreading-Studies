55fff69a2bb53d6e6888cc38dc70b5456ba57b95
Hasan Basri Aygümüş <hbasri2304@gmail.com>
2021-11-21 16:57:17 +0300

    Update Volatile.java

--- Multithreading/multiThreading02/Volatile.java
+++ Multithreading/multiThreading02/Volatile.java
@@ -21,7 +21,7 @@ public class Volatile {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(2500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
